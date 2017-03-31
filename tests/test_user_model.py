b9abd642422c8b19b62cc7849f4773129eba51cc
xiongsyao <xiongshengyao@hotmail.com>
2017-03-31 15:35:37 +0800

    add user_control

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,22 +1,70 @@
 import unittest
+import time
+from app import create_app, db
 from app.models import User
 
+
 class UserModelTestCase(unittest.TestCase):
-	def test_password_setter(self):
-		u = User(password = 'cat')
-		self.assertTrue(u.password_hash is not None)
+    def setUp(self):
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
 
-	def test_no_password_getter(self):
-		u = User(password = 'cat')
-		with self.assertRaises(AttributeError):
-			u.password
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
 
-	def test_password_vertification(self):
-		u = User(password = 'cat')
-		self.assertTrue(u.verify_password('cat'))
-		self.assertFalse(u.verify_password('dog'))
+    def test_password_setter(self):
+        u = User(password='cat')
+        self.assertTrue(u.password_hash is not None)
 
-	def test_password_salts_are_random(self):
-		u = User(password='cat')
-		u2 = User(password='cat')
-		self.assertTrue(u.password_hash != u2.password_hash)
+    def test_no_password_getter(self):
+        u = User(password='cat')
+        with self.assertRaises(AttributeError):
+            u.password
+
+    def test_password_verification(self):
+        u = User(password='cat')
+        self.assertTrue(u.verify_password('cat'))
+        self.assertFalse(u.verify_password('dog'))
+
+    def test_password_salts_are_random(self):
+        u = User(password='cat')
+        u2 = User(password='cat')
+        self.assertTrue(u.password_hash != u2.password_hash)
+
+    def test_valid_confirmation_token(self):
+        u = User(password='cat')
+        db.session.add(u)
+        db.session.commit()
+        token = u.generate_confirmation_token()
+        self.assertTrue(u.confirm(token))
+
+    def test_invalid_confirmation_token(self):
+        u1 = User(password='cat')
+        u2 = User(password='dog')
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.commit()
+        token = u1.generate_confirmation_token()
+        self.assertFalse(u2.confirm(token))
+
+    def test_expired_confirmation_token(self):
+        u = User(password='cat')
+        db.session.add(u)
+        db.session.commit()
+        token = u.generate_confirmation_token(1)
+        time.sleep(2)
+        self.assertFalse(u.confirm(token))
+
+    def test_roles_and_permissions(self):
+        Role.insert_roles()
+        u = User(email = 'xsy@example.com', password='password')
+        self.assertTrue(u.can(Permission.WRITE_ARTICLES))
+        self.assertFalse(u.can(Permissions.MODERATE_COMMENTS)
+
+    def test_anonymous_user(self):
+        u = AnonymousUser()
+        self.assertFalse(u.can(Permission,FOLLOW))
